1aafa6a7ab993b9fd35dd783b7a7b794addc7cb5
acpsandeepsingh <acpsandeepsingh@gmail.com>
2026-03-07 12:48:41 +0530

    build: fix local extractor include and drop redundant apk workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,13 +29,17 @@ dependencyResolutionManagement {
 }
 include (":app")
 
-// Use a local copy of NewPipe Extractor by uncommenting the lines below.
-// We assume, that NewPipe and NewPipe Extractor have the same parent directory.
+// Use a local copy of NewPipe Extractor when present.
+// We assume that NewPipe and NewPipe Extractor have the same parent directory.
 // If this is not the case, please change the path in includeBuild().
+val localExtractorPath = file("./NewPipeExtractor")
+val localExtractorSettings = file("./NewPipeExtractor/settings.gradle.kts")
 
-includeBuild("./NewPipeExtractor") {
-    dependencySubstitution {
-        substitute(module("com.github.TeamNewPipe:NewPipeExtractor"))
-            .using(project(":extractor"))
+if (localExtractorPath.isDirectory && localExtractorSettings.isFile) {
+    includeBuild("./NewPipeExtractor") {
+        dependencySubstitution {
+            substitute(module("com.github.TeamNewPipe:NewPipeExtractor"))
+                .using(project(":extractor"))
+        }
     }
 }
